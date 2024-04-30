b2b1f07ab89a26815250701c40ce3e04fa8cbbc4
nomino0302 <0302nomino@gmail.com>
2024-04-30 16:06:52 +0200

    Renamed assets

--- core/src/com/mygdx/game/Assets.java
+++ core/src/com/mygdx/game/Assets.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
+
 import java.util.HashMap;
 
 // Classe regroupant toutes les textures utilisées, on utilise un peu plus que 10 lignes pour mapper tout les assets
@@ -15,28 +16,15 @@ public class Assets {
         this.rockTexture = new Texture("objects/rock.png");
 
         // Textures du serpent, on utilise un dictionnaire pour séparer les textures bleues et rouges
+        String[] parts = new String[] {"headHaut", "headDroite", "headBas", "headGauche", // Textures têtes
+                "droiteHaut", "droiteBas", "gaucheHaut", "gaucheBas", "hori", "verti", "tailHaut", // Textures corps
+                "tailDroite", "tailBas", "tailGauche"}; // Textures queues
         this.snakeTexture.put("blue", new HashMap<>());
         this.snakeTexture.put("red", new HashMap<>());
-        for (String key: snakeTexture.keySet()) {
-            // Textures têtes
-            this.snakeTexture.get(key).put("headHaut", new Texture("snake/" + key + "/snake_head_haut.png"));
-            this.snakeTexture.get(key).put("headDroite", new Texture("snake/" + key + "/snake_head_droite.png"));
-            this.snakeTexture.get(key).put("headBas", new Texture("snake/" + key + "/snake_head_bas.png"));
-            this.snakeTexture.get(key).put("headGauche", new Texture("snake/" + key + "/snake_head_gauche.png"));
-
-            // Textures corps
-            this.snakeTexture.get(key).put("droiteHaut", new Texture("snake/" + key + "/snake_droite_haut.png"));
-            this.snakeTexture.get(key).put("droiteBas", new Texture("snake/" + key + "/snake_droite_bas.png"));
-            this.snakeTexture.get(key).put("gaucheHaut", new Texture("snake/" + key + "/snake_gauche_haut.png"));
-            this.snakeTexture.get(key).put("gaucheBas", new Texture("snake/" + key + "/snake_gauche_bas.png"));
-            this.snakeTexture.get(key).put("hori", new Texture("snake/" + key + "/snake_hori.png"));
-            this.snakeTexture.get(key).put("verti", new Texture("snake/" + key + "/snake_verti.png"));
-
-            // Textures queues
-            this.snakeTexture.get(key).put("tailHaut", new Texture("snake/" + key + "/snake_tail_haut.png"));
-            this.snakeTexture.get(key).put("tailDroite", new Texture("snake/" + key + "/snake_tail_droite.png"));
-            this.snakeTexture.get(key).put("tailBas", new Texture("snake/" + key + "/snake_tail_bas.png"));
-            this.snakeTexture.get(key).put("tailGauche", new Texture("snake/" + key + "/snake_tail_gauche.png"));
+        for (String color: snakeTexture.keySet()) {
+            for (String part: parts) {
+                this.snakeTexture.get(color).put(part, new Texture("snake/" + color + "/" + part + ".png"));
+            }
         }
     }
 }
