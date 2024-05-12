6488d7dbd14cdb14290fb2c652d51c9ddbff8a44
nomino0302 <0302nomino@gmail.com>
2024-05-12 17:05:05 +0200

    Patron IAG20.java

--- core/src/com/mygdx/game/IAG20.java
+++ core/src/com/mygdx/game/IAG20.java
@@ -1,12 +1,26 @@
 package com.mygdx.game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import java.util.Random;
 
-public class IAG20 {
-    public Random random;
+public class IAG20 extends Snake {
+    Random random;
 
-    public IAG20() {
+    IAG20(SpriteBatch batch, Assets assets, Scene scene, Board board, Objects objects, String side) {
+        super(batch, assets, scene, board, objects, side);
         this.random = new Random();
     }
 
+    public void evaluate(Snake snakeEnnemi) {
+        // Algo pour l'IA
 
+
+        // Fin de la fonction : elle change l'instance direction avec this.setDirection(...)
+    }
+
+    public void move(Snake snakeEnnemi, boolean grow) {
+        evaluate(snakeEnnemi);
+
+        super.move(grow);
+    }
 }
