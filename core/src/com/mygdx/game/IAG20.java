b8498713cfb495b7a3109ab6772a993e06314551
Eryze <luclin85@gmail.com>
2024-05-14 10:39:25 +0200

    truc mais pas sur

--- core/src/com/mygdx/game/IAG20.java
+++ core/src/com/mygdx/game/IAG20.java
@@ -1,24 +1,50 @@
 package com.mygdx.game;
+import java.util.ArrayList;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import java.util.Random;
 
 public class IAG20 extends Snake {
     Snake otherSnake;
-    Random random;
+    Board board;
+
 
     IAG20(SpriteBatch batch, Assets assets, Scene scene, Board board, Objects objects, String side) {
         super(batch, assets, scene, board, objects, side);
-
-        this.random = new Random();
-
+        this.otherSnake = null;
+        this.board = board;
     }
 
-    public void evaluate() {
+    void evaluate() {
         // Algo pour l'IA
+            ArrayList<String> possibleDirections = getPossibleDirections();
+            if (possibleDirections.isEmpty()) {
+                // Si toutes les directions mènent à une collision, choisir une direction aléatoire pour se sucider
+                possibleDirections = getAllDirections();
+            }
+            // Choisir la meilleure direction
+            String bestDirection = possibleDirections.get(0);
+            this.setDirection(bestDirection);
+        }
+        //Renvoi les directions possibles qui ne mènent pas à une collision.
 
-        // Fin de la fonction : elle change l'instance direction avec this.setDirection(...)
-    }
+        ArrayList<String> getPossibleDirections() {
+            ArrayList<String> directions = getAllDirections();
+            directions.removeIf(this::isBlocked);
+            return directions;
+        }
+        // Renvoi toutes les directions possibles.
+
+        ArrayList<String> getAllDirections() {
+            ArrayList<String> directions = new ArrayList<>();
+            directions.add(Global.HAUT);
+            directions.add(Global.BAS);
+            directions.add(Global.GAUCHE);
+            directions.add(Global.DROITE);
+            return directions;
+        }
+        boolean isBlocked(String direction) {
+            ArrayList<Integer> futureHead = futureHead(direction);
+            return board.usedPositions.contains(futureHead) || board.outsideLimits.contains(futureHead);
+        }
 
     @Override
     public boolean move(boolean grow) {
@@ -26,4 +52,5 @@ public class IAG20 extends Snake {
 
         return super.move(grow);
     }
+
 }
